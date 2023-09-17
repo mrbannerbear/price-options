d590d583d42041cb7faa9818298272d19417d1da
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2023-09-17 21:58:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='py-6 bg-neutral-200 min-h-screen text-black/80' style={{fontFamily: "work sans"}}>
+      <h1 className='text-center text-2xl font-semibold'>Price Options</h1>
+    </div>
+  )
+}
+
+export default App
